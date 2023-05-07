0b492b00b0265033bbdb528fe515cf6bee1d79d1
BloodSack <79668163+Oliversunblocked@users.noreply.github.com>
2023-05-07 06:35:01 -0500

    Add files via upload

---
+++ masterconnctoupdated.bat
@@ -0,0 +1,69 @@
+@echo off
+echo FAST LOADING BITCH  (ID: -DEVX-%random%-CRACKED-%random%-%random%-%random%-%random%-%random%-LOLS!)
+timeout 1 >nul /nobreak
+chcp 65001 >nul
+title %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+:login
+title %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+cls
+color 05
+echo  ▄████████    ▄████████    ▄████████  ▄████████    ▄█   ▄█▄    ▄████████ ████████▄       ▀█████████▄  ▄██   ▄        ████████▄     ▄████████  ▄█    █▄  ▀████    ▐████▀ 
+echo ███    ███   ███    ███   ███    ███ ███    ███   ███ ▄███▀   ███    ███ ███   ▀███        ███    ███ ███   ██▄      ███   ▀███   ███    ███ ███    ███   ███▌   ████▀  
+echo ███    █▀    ███    ███   ███    ███ ███    █▀    ███▐██▀     ███    █▀  ███    ███        ███    ███ ███▄▄▄███      ███    ███   ███    █▀  ███    ███    ███  ▐███    
+echo ███         ▄███▄▄▄▄██▀   ███    ███ ███         ▄█████▀     ▄███▄▄▄     ███    ███       ▄███▄▄▄██▀  ▀▀▀▀▀▀███      ███    ███  ▄███▄▄▄     ███    ███    ▀███▄███▀    
+echo ███        ▀▀███▀▀▀▀▀   ▀███████████ ███        ▀▀█████▄    ▀▀███▀▀▀     ███    ███      ▀▀███▀▀▀██▄  ▄██   ███      ███    ███ ▀▀███▀▀▀     ███    ███    ████▀██▄     
+echo ███    █▄  ▀███████████   ███    ███ ███    █▄    ███▐██▄     ███    █▄  ███    ███        ███    ██▄ ███   ███      ███    ███   ███    █▄  ███    ███   ▐███  ▀███    
+echo ███    ███   ███    ███   ███    ███ ███    ███   ███ ▀███▄   ███    ███ ███   ▄███        ███    ███ ███   ███      ███   ▄███   ███    ███ ███    ███  ▄███     ███▄  
+echo ████████▀    ███    ███   ███    █▀  ████████▀    ███   ▀█▀   ██████████ ████████▀       ▄█████████▀   ▀█████▀       ████████▀    ██████████  ▀██████▀  ████       ███▄ 
+echo              ███    ███                           ▀                                                                                                                                                 
+echo.                                             
+echo.                                                       (WE CRACKED THIS NIGGAS ADMIN BUILD LMAO!) SMOKING THAT PACK!
+echo                               (GET FUCKED RETARD WE CRACKED YOUR SHIT PUSSY)    (TYPE ANY PASSWORD IT WORKS LOL)          SPOOFER VERSION: 2.76.9-ADMIN-BUILD  UPDATES: NONE
+set /p user=Username (root):
+echo.
+set /p pass=Password (root):
+if %user% == a if %pass% == a goto main
+:refresh
+title %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+cls
+color 09
+echo  █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗    ██████╗  █████╗  ██████╗ ███████╗
+echo ██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║    ██╔══██╗██╔══██╗██╔════╝ ██╔════╝
+echo ███████║██║  ██║██╔████╔██║██║██╔██╗ ██║    ██████╔╝███████║██║  ███╗█████╗  
+echo ██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║    ██╔═══╝ ██╔══██║██║   ██║██╔══╝  
+echo ██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║    ██║     ██║  ██║╚██████╔╝███████╗
+echo.
+echo.
+echo SERVER: 10.582.683
+echo ECHO HOST: 92862123
+echo SPOOFER: TRACK 1
+echo WEBHOOK STATUS: OFFLINE/DELETED = ERROR 850 (WEBHOOK DELETED) (CHECKED 20 SECONDS AGO) 
+:main
+title %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+cls
+color 0a
+echo.
+echo ▄██   ▄    ▄██████▄  ███    █▄          ▄██████▄   ▄██████▄      ███           ▄████████    ▄████████    ▄████████  ▄████████    ▄█   ▄█▄    ▄████████ ████████▄  
+echo ███   ██▄ ███    ███ ███    ███        ███    ███ ███    ███ ▀█████████▄      ███    ███   ███    ███   ███    ███ ███    ███   ███ ▄███▀   ███    ███ ███   ▀███ 
+echo ███▄▄▄███ ███    ███ ███    ███        ███    █▀  ███    ███    ▀███▀▀██      ███    █▀    ███    ███   ███    ███ ███    █▀    ███▐██▀     ███    █▀  ███    ███ 
+echo ▀▀▀▀▀▀███ ███    ███ ███    ███       ▄███        ███    ███     ███   ▀      ███         ▄███▄▄▄▄██▀   ███    ███ ███         ▄█████▀     ▄███▄▄▄     ███    ███ 
+echo ▄██   ███ ███    ███ ███    ███      ▀▀███ ████▄  ███    ███     ███          ███        ▀▀███▀▀▀▀▀   ▀███████████ ███        ▀▀█████▄    ▀▀███▀▀▀     ███    ███ 
+echo ███   ███ ███    ███ ███    ███        ███    ███ ███    ███     ███          ███    █▄  ▀███████████   ███    ███ ███    █▄    ███▐██▄     ███    █▄  ███    ███ 
+echo ███   ███ ███    ███ ███    ███        ███    ███ ███    ███     ███          ███    ███   ███    ███   ███    ███ ███    ███   ███ ▀███▄   ███    ███ ███   ▄███ 
+echo  ▀█████▀   ▀██████▀  ████████▀         ████████▀   ▀██████▀     ▄████▀        ████████▀    ███    ███   ███    █▀  ████████▀    ███   ▀█▀   ██████████ ████████▀  
+echo                                                                                            ███    ███                           ▀                                 
+echo                        (ID: -DEVX-%random%-CRACKED-%random%-%random%-%random%-%random%-%random%-LOLS!) Your id gets spoofed every relaunch, dont worry about id lolz)
+echo.
+echo   █████╗ ██╗     ███████╗ ██████╗     ██╗   ██╗ ██████╗ ██╗   ██╗    ██╗     ██╗██╗  ██╗███████╗    ████████╗██╗  ██╗███████╗    ██████╗ ██████╗  ██████╗ ███████╗██████╗ 
+echo  ██╔══██╗██║     ██╔════╝██╔═══██╗    ╚██╗ ██╔╝██╔═══██╗██║   ██║    ██║     ██║██║ ██╔╝██╔════╝    ╚══██╔══╝██║  ██║██╔════╝    ██╔══██╗██╔══██╗██╔═══██╗██╔════╝╚════██╗
+echo  ███████║██║     ███████╗██║   ██║     ╚████╔╝ ██║   ██║██║   ██║    ██║     ██║█████╔╝ █████╗         ██║   ███████║█████╗      ██║  ██║██║  ██║██║   ██║███████╗  ▄███╔╝
+echo  ██╔══██║██║     ╚════██║██║   ██║      ╚██╔╝  ██║   ██║██║   ██║    ██║     ██║██╔═██╗ ██╔══╝         ██║   ██╔══██║██╔══╝      ██║  ██║██║  ██║██║   ██║╚════██║  ▀▀══╝ 
+echo  ██║  ██║███████╗███████║╚██████╔╝       ██║   ╚██████╔╝╚██████╔╝    ███████╗██║██║  ██╗███████╗       ██║   ██║  ██║███████╗    ██████╔╝██████╔╝╚██████╔╝███████║  ██╗   
+echo  ╚═╝  ╚═╝╚══════╝╚══════╝ ╚═════╝        ╚═╝    ╚═════╝  ╚═════╝     ╚══════╝╚═╝╚═╝  ╚═╝╚══════╝       ╚═╝   ╚═╝  ╚═╝╚══════╝    ╚═════╝ ╚═════╝  ╚═════╝ ╚══════╝  ╚═╝   
+echo      (NIGGA NEEDED HELP WITH TRACK 1)     (Press Enter To Spoof CONSOLE NAME)          (ALSO THIS NIGGA HAD A WEBHOOK LOGGER LOL)   (REVILO CRACKED THIS SHIT PUSSY)      revilo#0011       (DDOS HIM AND HIS WEBSERVER) (I REMOVED THE WEBHOOK SO ITS GOOD + I ADDED MY OWN CUSTOM SERVER JUST TO MAKE SURE)                                                                                                                                                      
+:home
+set /p home=Enter A Command (help):
+if %home% == clear goto main
+if %home% == Spoof start gorillatagspoofed
+if %home% == c goto login
+if %home% == refresh goto refresh
